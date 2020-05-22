73b746d4379efd08b9ba96a44c73583d5f0b9bdc
Grog4r <nk99nick@gmx.de>
2020-05-22 12:29:11 +0200

    Commit

--- src/AuswahlProblem/Main.java
+++ src/AuswahlProblem/Main.java
@@ -178,10 +178,13 @@ public class Main {
 	public static void clearScreen() {
 		// Clears Screen in java
 		try {
-			if (System.getProperty("os.name").contains("Windows"))
+			if (System.getProperty("os.name").contains("Windows")) {
 				new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-			else
-				Runtime.getRuntime().exec("clear");
+			}
+			else {
+				System.out.print("\033[H\033[2J");
+				System.out.flush();
+			}
 		} catch (IOException | InterruptedException ex) {
 			ex.printStackTrace();
 		}
